8c6dd504a00d011f90d365f1f9b7f3118d7d54c2
rodrigo792 <rodrigoalvarenga790@gmail.com>
2025-03-04 18:16:53 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+       
+        int n = int.Parse(Console.ReadLine());
+
+        
+        for (int i = 2; i <= n; i += 2) 
+        {
+            int quadrado = i * i;
+            Console.WriteLine($"{i}^2 = {quadrado}");
+        }
+    }
+}
+
